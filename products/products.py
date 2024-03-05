b912688b9f1f09cf5a5fdb7e61a7f0bd1495613d
Norbert20037 <94049804+Norbert20037@users.noreply.github.com>
2024-03-05 13:50:15 +0100

    update 5.3.2024

--- products/products.py
+++ products/products.py
@@ -3,7 +3,9 @@ from API.api import GetAllProducts, GetSingleProducts, GetAllProductsCategory
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
-
+@products_bp.route('/products/add')
+def addProduct():
+    return render_template('products/new-product.html')
 @products_bp.route('/products')
 def index():
     data = GetAllProducts()
